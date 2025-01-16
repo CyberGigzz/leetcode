ab1b27c994452323d28b84544792de438b946792
Giga Katamadze <gigakatamadze0@gmail.com>
2025-01-16 13:31:51 +0400

    solution for add two integer and reformatting

---
+++ problems_2000-3000/2235-AddTwoIntegers/java/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.sum(1, 2));
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
