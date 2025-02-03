2c6f03f02e6e5a54421502bac7be4398d3cd6f24
Giga Katamadze <gigakatamadze0@gmail.com>
2025-02-03 15:19:07 +0400

    solution for 344 reverse string

---
+++ problems_0-1000/344-ReverseString/python/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
+
+# Example usage
+if __name__ == "__main__":
+    example_string = ["h", "e", "l", "l", "o"]
+    s = Solution()
+    
+    # Call the reverseString method to modify the list in-place
+    s.reverseString(example_string)
+    
+    # Print the modified list
+    print(example_string)  # Output: ['o', 'l', 'l', 'e', 'h']
