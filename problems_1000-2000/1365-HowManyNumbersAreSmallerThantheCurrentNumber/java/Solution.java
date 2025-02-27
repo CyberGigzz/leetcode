3a98e97fecf2e3d9e83051f2e67f210bccc4a902
Giga Katamadze <gigakatamadze0@gmail.com>
2025-02-27 23:56:31 +0400

    Solution for 1365 problem in java

---
+++ problems_1000-2000/1365-HowManyNumbersAreSmallerThantheCurrentNumber/java/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int maxValue = 0;
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+
+        return null;
+    }
+}
